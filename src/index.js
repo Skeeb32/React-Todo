0b17d6b3eb42cb9f3dda3cb38b2ba713ddcb420a
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2019-12-02 16:05:10 -0800

    ToDo Project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
 import React from 'react';
-import { render } from 'react-dom';
-import App from './App';
+import ReactDOM from "react-dom";
+import App from "./App"
 
-render(<App />, document.getElementById('root'));
+const data = [
+    {
+      name: "Clean Room",
+      id: 1,
+      completed: false
+    },
+    {
+      name: "Buy Groceries",
+      id: 2,
+      completed: false
+    },
+    {
+      name: "Wash Car",
+      id: 3,
+      completed: false
+    },
+    {
+      name: "Go For A Walk",
+      id: 4,
+      completed: false
+    },
+    {
+      name: "Study",
+      id: 5,
+      completed: false
+    },
+    {
+      name: "Call Parents",
+      id: 6,
+      completed: false
+    }
+  ];
+export default data;
+
+ 
+        
+        
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
